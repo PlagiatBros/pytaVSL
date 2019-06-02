f302f99e0176f19cc8ca3c5f0d0dc2097663d8d3
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2019-06-02 14:36:59 +0200

    change debug cli option

--- pytaVSL.py
+++ pytaVSL.py
@@ -12,12 +12,12 @@ parser.add_argument('--fps',  help='maximum framerate', type=int, default=25)
 parser.add_argument('--max-gpu',  help='maximum gpu memory (in MB)', type=int, default=64)
 parser.add_argument('--fullscreen',  help='launch in fullscreen', default=False, action='store_true')
 parser.add_argument('--api',  help='print osc api and exit', default=False, action='store_true')
-parser.add_argument('--log',  help='verbosity level', default='warning', choices=['debug', 'info', 'warning', 'error', 'critical'])
+parser.add_argument('--debug',  help='print debug logs', default=False, action='store_true')
 
 args = parser.parse_args()
 
 from pi3d import Log
-Log(name=None, level=args.log.upper())
+Log(name=None, level='DEBUG' if args.debug else 'WARNING')
 
 from engine import PytaVSL
 pyta = PytaVSL(name=args.name, port=args.port, files=args.load, fps=args.fps, fullscreen=args.fullscreen)
