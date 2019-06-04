2eea418776a263e2e7144c5d667410648523b1ef
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2019-06-04 11:24:35 +0200

    oo video

--- video.py
+++ video.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import pi3d
 from pi3d.Display import Display
-from pi3d.constants import opengles
 from utils import unicode
 from osc import osc_method
 
@@ -15,70 +14,87 @@ import subprocess
 import threading
 import time
 import numpy
+import re
 
-def read_video(file, h, w, p):
+videos_formats = re.compile('^.*\.(mov|avi|mpg|mpeg|mp4|mkv|wmv|webm)$')
 
-    player = {
-        'frame': None,
-        'new': False,
-        'restart': True
-    }
+class Player(object):
+    def __init__(self, file):
 
-    def pipe_thread():
-        pipe = None
+        self.resolution = self.get_video_resolution(file)
+        self.depth = 8
+        self.array_shape = (self.resolution[1], self.resolution[0], int(self.depth / 8))
+
+        self.frame = None
+        self.new_frame = False
+        self.process = None
+        self.running = False
+        self.bufsize = int(self.resolution[0] * self.resolution[1] * self.depth / 8)
+        self.command = [ 'ffmpeg', '-i', file, '-f', 'image2pipe', '-r', '25',
+                                  '-pix_fmt', 'rgb' + str(self.depth), '-vcodec', 'rawvideo', '-']
+
+        t = threading.Thread(target=self.loop)
+        t.daemon = True
+        t.start()
+
+    def loop(self):
 
         while True:
 
-            if player['restart']:
-                if pipe is not None:
-                    pipe.terminate()
+            if self.running:
 
-                command = [ 'ffmpeg', '-i', file, '-f', 'image2pipe', '-r', '25',
-                                          '-pix_fmt', 'rgb8', '-vcodec', 'rawvideo', '-']
-                pipe = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=w*h*p)
-                player['restart'] = False
+                self.new_frame = False
 
-            player['new'] = False
-            frame =  pipe.stdout.read(h * w * p)
-            pipe.stdout.flush()
-            pipe.stderr.flush()
+                frame = self.process.stdout.read(self.bufsize)
 
-            if len(frame) < h * w * p:
-                player['restart'] = True
-            elif frame != player['frame']:
-                player['frame'] = frame
-                player['new'] = True
+                self.process.stdout.flush()
+                self.process.stderr.flush()
+
+                if len(frame) < self.bufsize:
+                    self.start()
+                elif frame != self.frame:
+                    self.frame = frame
+                    self.new_frame = True
 
             time.sleep(0.04)
 
-    t = threading.Thread(target=pipe_thread)
-    t.daemon = True
-    t.start()
+    def start(self):
+        if self.process:
+            self.stop()
+        self.process = subprocess.Popen(self.command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=self.bufsize)
+        self.running = True
 
-    return player
+    def stop(self):
+        self.process.terminate()
+        self.running = False
+        self.new_frame = False
 
+    def get_frame_array(self):
+        array = numpy.fromstring(self.frame, dtype='uint8')
+        array.shape = self.array_shape
+        return array
 
-def get_video_resolution(file):
-    out = subprocess.check_output('ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0'.split(' ') + [file])
-    res = out.split(b'\n')[0]
-    res = res.split(b'x')
-    res = [int(x) for x in res]
-    return res
+    def get_video_resolution(self, file):
+        out = subprocess.check_output('ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0'.split(' ') + [file])
+        res = out.split(b'\n')[0]
+        res = res.split(b'x')
+        res = [int(x) for x in res]
+        return res
 
 class Video(object):
 
     def __init__(self, texture, *args, **kwargs):
 
         self.video = False
-        self.video_reader = 0
+        self.video_player = None
 
-        if isinstance(texture, (str, unicode)) and '.mp4' in texture:
-            LOGGER.warning('video support is experimental !')
-            w, h = get_video_resolution(texture)
-            self.video_resolution = (h, w, 1)
-            self.video_reader = read_video(texture, *self.video_resolution)
-            texture = pi3d.Texture(numpy.zeros(self.video_resolution, dtype='uint8'))
-            self.video = True
+        if isinstance(texture, (str, unicode)):
+            match = videos_formats.match(texture)
+            if match and match.string:
+                LOGGER.warning('video support is experimental !')
+                self.video_player = Player(texture)
+                texture = pi3d.Texture(numpy.zeros(self.video_player.array_shape, dtype='uint8'))
+                self.video = True
 
         super(Video, self).__init__(texture=texture, *args, **kwargs)
 
@@ -90,7 +106,7 @@ class Video(object):
 
     def video_next_frame(self):
         if self.video and self.visible:
-            if self.video_reader['new']:
-                array = numpy.fromstring(self.video_reader['frame'], dtype='uint8')
-                array.shape = self.video_resolution
-                self.buf[0].textures[0].update_ndarray(array)
+            if not self.video_player.running:
+                self.video_player.start()
+            if self.video_player.new_frame:
+                self.buf[0].textures[0].update_ndarray(self.video_player.get_frame_array())
