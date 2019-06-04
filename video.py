22dd73b3a9d9ba51140554a5ae0b372106ee2d7e
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2019-06-04 07:41:19 +0200

    video decode with ffmpeg

--- video.py
+++ video.py
@@ -8,22 +8,24 @@ from pi3d.constants import opengles
 from utils import unicode
 from osc import osc_method
 
-import numpy as np
+import logging
+LOGGER = logging.getLogger(__name__)
 
 class Video(object):
 
     def __init__(self, texture, *args, **kwargs):
 
         self.video = False
-        self.video_start = 0
+        self.video_reader = 0
 
         if isinstance(texture, (str, unicode)) and '.mp4' in texture:
-            import cv2
-            self.video = cv2.VideoCapture(texture)
-            texture = pi3d.Texture(self.video.read()[1])
+            import imageio
+            LOGGER.warning('video support is experimental !')
+            self.video_reader = imageio.get_reader(texture,  'ffmpeg', loop=True)
+            texture = pi3d.Texture(self.video_reader.get_next_data())
+            self.video = True
 
         super(Video, self).__init__(texture=texture, *args, **kwargs)
-
 
     def draw(self, *args, **kwargs):
 
@@ -33,8 +35,4 @@ class Video(object):
 
     def video_next_frame(self):
         if self.video and self.visible:
-            (ok, frame) = self.video.read()
-            if ok:
-                self.buf[0].textures[0].update_ndarray(frame)
-            else:
-                self.video.set(0, 0)
+            self.buf[0].textures[0].update_ndarray(self.video_reader.get_next_data())
