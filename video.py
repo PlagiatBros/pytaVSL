1ba296f466684714ef49d888b8a5933bff5bbb89
bordun <jean-emmanuel.doucet@groolot.net>
2019-06-04 19:45:39 +0200

    more accurate video timing

--- video.py
+++ video.py
@@ -38,12 +38,11 @@ class Video(object):
                 self.video_reader = cv2.VideoCapture(texture)
                 self.video_shape = (int(self.video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(self.video_reader.get(cv2.CAP_PROP_FRAME_WIDTH)), 3)
                 self.video_texture = pi3d.Texture(numpy.zeros(self.video_shape, dtype='uint8'))
-                self.video_length = self.video_reader.get(cv2.CAP_PROP_FRAME_COUNT)
-                self.video_fps = min(self.video_reader.get(cv2.CAP_PROP_FPS), 60)
-                self.video_frame_duration = 1. / self.video_fps
+
+                self.video_frame_duration = 1. / min(self.video_reader.get(cv2.CAP_PROP_FPS), 60)
                 self.video_speed = 1.0
-                self.video_frame = 0
-                self.video_changed_time = 0
+                self.video_elapsed_time = 0
+                self.video_time = 0
 
                 texture = self.video_texture
 
@@ -57,35 +56,14 @@ class Video(object):
         super(Video, self).draw(*args, **kwargs)
 
 
-    @osc_property('video_frame', 'video_frame')
-    def set_video_frame(self, frame):
+    @osc_property('video_time', 'video_time')
+    def set_video_time(self, time):
+        self.video_reader.set(cv2.CAP_PROP_POS_MSEC, float(time) * 1000)
+        self.video_time = self.video_reader.get(cv2.CAP_PROP_POS_MSEC) / 1000.
 
-        new_frame = int(frame) % self.video_length
+        if self.video_time > 0:
+            self.video_elapsed_time = Display.INSTANCE.time
 
-        if new_frame == self.video_frame:
-
-            return
-
-        elif new_frame < self.video_frame:
-
-            self.video_reader.set(cv2.CAP_PROP_POS_MSEC, new_frame * self.video_frame_duration * 1000) # faster ?
-            # self.video_reader.set(cv2.CAP_PROP_POS_FRAMES, new_frame)
-            self.video_reader.grab()
-            self.video_frame = new_frame
-
-        else:
-
-            while new_frame > self.video_frame :
-                self.video_frame += 1
-                ok = self.video_reader.grab()
-                if not ok:
-                    self.set_video_frame(0)
-                    break
-
-    # @osc_property('video_time', 'video_time')
-    # def set_video_time(self, time):
-    #     self.video_reader.set(cv2.CAP_PROP_POS_MSEC, float(time * 1000))
-    #     self.video_frame = self.video_reade.get(cv2.CAP_PROP_POS_FRAMES)
 
     @osc_property('video_speed', 'video_speed')
     def set_video_speed(self, speed):
@@ -93,33 +71,32 @@ class Video(object):
 
     def video_next_frame(self):
 
-        now = Display.INSTANCE.time
+        time = Display.INSTANCE.time
 
-        if self.video_changed_time is 0 or self.video_speed == 0:
-            self.video_changed_time = now
+        if self.video_elapsed_time is 0 or self.video_speed == 0:
+            self.video_elapsed_time = time
+            if self.video_speed == 0:
+                return
 
-        if self.video_speed == 0:
-            return
+        delta = (time - self.video_elapsed_time)
+        frames = int(delta / (self.video_frame_duration / self.video_speed))
 
-        current_frame = self.video_frame
-        elapsed = now - self.video_changed_time
-        duration = self.video_frame_duration
-        duration = max(duration / abs(self.video_speed), 1. / Display.INSTANCE.frames_per_second / 1000)
+        if frames > 0:
 
-        while elapsed >= duration:
+            for i in range(frames):
 
-            elapsed = elapsed - duration
-            current_frame = current_frame + 1
+                ok = self.video_reader.grab()
+                if not ok:
+                    self.set_video_time(0)
 
-        if current_frame != self.video_frame:
-
-            self.video_changed_time = now + elapsed
-            self.set_video_frame(current_frame)
+            self.video_elapsed_time += frames * (self.video_frame_duration / self.video_speed)
             self.video_load_current_frame()
 
+            self.video_time = self.video_reader.get(cv2.CAP_PROP_POS_MSEC) / 1000.
 
 
     def video_load_current_frame(self):
         ok, frame = self.video_reader.retrieve()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        self.video_texture.update_ndarray(frame)
+        if ok:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            self.video_texture.update_ndarray(frame)
