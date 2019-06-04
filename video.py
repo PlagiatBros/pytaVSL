02b3131cfd60710882ced8c895b1872433d5abe4
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2019-06-04 10:53:25 +0200

    video playback with ffmpeg only

--- video.py
+++ video.py
@@ -11,6 +11,60 @@ from osc import osc_method
 import logging
 LOGGER = logging.getLogger(__name__)
 
+import subprocess
+import threading
+import time
+import numpy
+
+def read_video(file, h, w, p):
+
+    player = {
+        'frame': None,
+        'new': False,
+        'restart': True
+    }
+
+    def pipe_thread():
+        pipe = None
+
+        while True:
+
+            if player['restart']:
+                if pipe is not None:
+                    pipe.terminate()
+
+                command = [ 'ffmpeg', '-i', file, '-f', 'image2pipe', '-r', '25',
+                                          '-pix_fmt', 'rgb8', '-vcodec', 'rawvideo', '-']
+                pipe = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=w*h*p)
+                player['restart'] = False
+
+            player['new'] = False
+            frame =  pipe.stdout.read(h * w * p)
+            pipe.stdout.flush()
+            pipe.stderr.flush()
+
+            if len(frame) < h * w * p:
+                player['restart'] = True
+            elif frame != player['frame']:
+                player['frame'] = frame
+                player['new'] = True
+
+            time.sleep(0.04)
+
+    t = threading.Thread(target=pipe_thread)
+    t.daemon = True
+    t.start()
+
+    return player
+
+
+def get_video_resolution(file):
+    out = subprocess.check_output('ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0'.split(' ') + [file])
+    res = out.split(b'\n')[0]
+    res = res.split(b'x')
+    res = [int(x) for x in res]
+    return res
+
 class Video(object):
 
     def __init__(self, texture, *args, **kwargs):
@@ -19,10 +73,11 @@ class Video(object):
         self.video_reader = 0
 
         if isinstance(texture, (str, unicode)) and '.mp4' in texture:
-            import imageio
             LOGGER.warning('video support is experimental !')
-            self.video_reader = imageio.get_reader(texture,  'ffmpeg', loop=True)
-            texture = pi3d.Texture(self.video_reader.get_next_data())
+            w, h = get_video_resolution(texture)
+            self.video_resolution = (h, w, 1)
+            self.video_reader = read_video(texture, *self.video_resolution)
+            texture = pi3d.Texture(numpy.zeros(self.video_resolution, dtype='uint8'))
             self.video = True
 
         super(Video, self).__init__(texture=texture, *args, **kwargs)
@@ -35,4 +90,7 @@ class Video(object):
 
     def video_next_frame(self):
         if self.video and self.visible:
-            self.buf[0].textures[0].update_ndarray(self.video_reader.get_next_data())
+            if self.video_reader['new']:
+                array = numpy.fromstring(self.video_reader['frame'], dtype='uint8')
+                array.shape = self.video_resolution
+                self.buf[0].textures[0].update_ndarray(array)
