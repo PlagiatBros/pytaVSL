5528200f42fefe3f72866bf0c6f4f029f360e26e
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2019-06-12 11:46:57 +0200

    avoid mask computing when not needed

--- shaders/effects/mask.fs
+++ shaders/effects/mask.fs
@@ -1,11 +1,16 @@
 float maskState = unif[19][0];
-float maskHardness = unif[19][1];
-float maskThresh = unif[19][2];
 
-vec4 maskColor = texture2D(tex1, maskCoords);
-float maskAlpha = distance(maskColor, vec4(0.0, 0.0, 0.0, 1.0));
+if (maskState == 1.0) {
 
-maskAlpha += step(maskThresh, maskAlpha);
-maskAlpha *= step(min(maskHardness, 1.0), maskAlpha);
+    float maskHardness = unif[19][1];
+    float maskThresh = unif[19][2];
 
-gl_FragColor.a = gl_FragColor.a * (1.0 - maskState + maskAlpha * maskState);
+    vec4 maskColor = texture2D(tex1, maskCoords);
+    float maskAlpha = distance(maskColor, vec4(0.0, 0.0, 0.0, 1.0));
+
+    maskAlpha += step(maskThresh, maskAlpha);
+    maskAlpha *= step(min(maskHardness, 1.0), maskAlpha);
+
+    gl_FragColor.a = gl_FragColor.a * maskAlpha;
+
+}
