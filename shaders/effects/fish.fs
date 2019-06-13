5df30ac905072aa1346064d5cff095671140195a
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2019-06-13 18:39:14 +0200

    update fish

--- shaders/effects/fish.fs
+++ shaders/effects/fish.fs
@@ -1,11 +1,14 @@
 vec2 m = vec2(0.5, 0.5);
 vec2 d = coords-m;
 float r = sqrt(dot(d, d));
-float power = unif[16][1];
+float power = unif[16][1] * 2.2;
+float bind;
 vec2 uv = coords;
 if (power > 0.0) {
-    uv = m +  normalize(d) * tan(r * power * 3.0) * 0.5 / tan(0.5* power * 3.0) ;
+    bind = sqrt(dot(m, m));
+    uv = m +  normalize(d) * tan(r * power) * bind / tan(bind * power) ;
 } else {
-    uv = m +  normalize(d) * atan(r * power * 100.0) * 0.5 / atan(0.5* power * 100.0) ;
+    bind = 0.5;
+    uv = m +  normalize(d) * atan(r * power) * bind / atan(bind * power) ;
 }
 vec2 coords = uv;
