176f95e26e5afbbca7b1714418c5552efb203fe8
bordun <jean-emmanuel.doucet@groolot.net>
2019-10-22 16:16:14 +0200

    group: add group effects (via local post_process)

--- slides/group.py
+++ slides/group.py
@@ -14,6 +14,8 @@ class Group(object):
         self.sequence_index = 0
         self.sequence_normal_index = 0
 
+        self.post_process = None
+
         super(Group, self).__init__(*args, **kwargs)
 
     def draw(self, *args, **kwargs):
@@ -27,7 +29,34 @@ class Group(object):
                 if self.is_sequence:
                     self.set_sequence_index(self.sequence_index)
 
+            if self.is_group and self.active_effects:
+                self.post_process.unif[:] = self.unif[:]
+                self.post_process.buf[0].unib[:] = self.buf[0].unib[:]
+                self.post_process.set_visible(1)
+                self.post_process.capture_start()
+
             super(Group, self).draw(*args, **kwargs)
+
+            if self.is_group and self.active_effects:
+                self.post_process.capture_end()
+                self.post_process.draw()
+
+    def toggle_effect(self, *args, **kwargs):
+        """
+        override Effect.toggle_effect to copy shaders to group post_process
+        """
+        super(Group, self).toggle_effect(*args, **kwargs)
+
+        if self.is_group and (self.active_effects or self.post_process):
+
+            if not self.post_process:
+                from postprocess import PostProcess
+                self.post_process = PostProcess(self.parent)
+
+            self.post_process.toggle_effect(*args, **kwargs)
+
+            if self.effect_mask != self.post_process.effect_mask:
+                self.post_process.set_effect_mask(self.effect_mask)
 
     @osc_property('sequence_mode', 'is_sequence')
     def set_sequence_mode(self, mode):
