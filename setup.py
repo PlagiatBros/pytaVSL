16c4ff228300b78120b408adb971ba3910ce769f
bordun <jean-emmanuel.doucet@groolot.net>
2020-05-08 18:39:44 +0200

    package: clean

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
+from sys import path
+
+path.insert(0, './')
 
 from pytaVSL import __version__
 
 setup(
     name='pytaVSL',
     packages=['pytaVSL', 'pytaVSL.engine', 'pytaVSL.fonts', 'pytaVSL.shaders', 'pytaVSL.slides'],
-    package_data={'pytaVSL': ['shaders/**/*', 'fonts/*']},
+    package_data={'pytaVSL': ['shaders/*', 'shaders/**/*', 'fonts/*']},
     version=__version__,
     description='python tiny aproximative Virtual Stage Light',
     url='https://github.com/PlagiatBros/pytaVSL',
