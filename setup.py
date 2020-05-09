8c0778e1a2bb93bf3f97b050deeade2c9b5c2c1c
bordun <jean-emmanuel.doucet@groolot.net>
2020-05-09 12:15:28 +0200

    package: post install print video extra dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+
 from sys import path
 
 path.insert(0, './')
 
 from pytaVSL import __version__
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        print('==========')
+        print('For video playback support you need to install the following:')
+        print('- opencv (pip: opencv-python / debian: python3-opencv)')
+        print('- mplayer.py (pip: player.py)')
+        print('- mplayer')
+        print('- ffmpeg')
+        print('==========')
 
 setup(
     name='pytaVSL',
@@ -20,10 +34,14 @@ setup(
     install_requires=[
         'pi3d==2.36',
         'toml==0.10',
-        'liblo==0.10'
+        'liblo==0.10',
+
     ],
     extras_require={
         'video': ['opencv-python'],
         'video_audio': ['opencv-python', 'mplayer.py']
+    },
+    cmdclass={
+        'install': PostInstallCommand,
     }
 )
